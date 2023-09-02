64a546bb65b5762be3b1b7065efee8093d471674
VadersFist0501 <1of4boys@gmail.com>
2023-09-01 18:32:26 -0600

    all

---
+++ src/Attack.java
@@ -0,0 +1,11 @@
+public class Attack {
+
+    private enum AttackType {
+        SPLASH_AOE,
+        SINGLE_TARGET,
+        CHAIN_LIGHTING
+    };
+    private String name;
+    private int baseDmg;
+
+}
